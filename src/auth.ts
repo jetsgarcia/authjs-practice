16c70a11e6671c97fc8ed690815b4b86a966e9fe
Jetter <jlgarcia2@student.apc.edu.ph>
2025-03-03 20:09:02 +0800

    Refactor auth file by renaming variable db to prisma and by using dot notation when referring to parameter values

--- src/auth.ts
+++ src/auth.ts
@@ -1,36 +1,35 @@
 import NextAuth from "next-auth";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import Credentials from "next-auth/providers/credentials";
-import { db } from "./lib/db";
+import { prisma } from "@/lib/prisma";
 import bcrypt from "bcrypt";
 
-type CredentialsType = {
-  email: string;
-  password: string;
-};
-
-export const { handlers, signIn, signOut, auth } = NextAuth({
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  session: { strategy: "jwt" },
   providers: [
     Credentials({
       credentials: {
         email: { label: "Email", type: "email" },
         password: { label: "Password", type: "password" },
       },
-      authorize: async (credentials) => {
-        const { email, password } = credentials as CredentialsType;
-
-        if (!email || !password) {
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
           return null;
         }
 
-        const user = await db.user.findUnique({
-          where: { email: email },
+        const user = await prisma.user.findUnique({
+          where: { email: credentials.email as string },
         });
 
         if (!user || !user.password) {
           return null;
         }
 
-        const passwordMatch = await bcrypt.compare(password, user.password);
+        const passwordMatch = await bcrypt.compare(
+          credentials.password as string,
+          user.password
+        );
 
         if (!passwordMatch) {
           return null;
